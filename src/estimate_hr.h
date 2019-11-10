06c700f761753dfc9b38f8126f9f285289ec4e0e
Andrew Casper <andrew.jacob.casper@gmail.com>
2019-11-10 14:26:26 -0600

    Working basic heart rate detector.

--- src/estimate_hr.h
+++ src/estimate_hr.h
@@ -8,6 +8,7 @@ namespace argos {
 typedef struct {
   int sample_rate;
   float std_dev_threshold;
+  float max_heart_rate;
 } hr_params_s;
 
 int EstimateHeartRate(const std::vector<int>& data, const hr_params_s& hr_param);
--- src/main.cc
+++ src/main.cc
@@ -3,21 +3,24 @@
 
 #include "estimate_hr.h"
 
+extern int heartbeat_01[3570];
+
 int main(void) {
 
   // Creates simple data
   std::vector<int> data;
-  for(int ii = 0; ii < 100; ii++){
-    data.push_back(ii);
+  for(int ii = 0; ii < sizeof(heartbeat_01)/4; ii++){
+    data.push_back(heartbeat_01[ii]);
   }
+  std::cout << "Loaded a " << data.size() << " length data set." << std::endl;
   argos::hr_params_s hr_params;
   hr_params.sample_rate = 250;
   hr_params.std_dev_threshold = 3.0;
-
+  hr_params.max_heart_rate = 275;
   int hr;
   hr = argos::EstimateHeartRate(data, hr_params);
 
-  std::cout << "Estimated Heart Rate: " << hr << std::endl;
+  std::cout << "Estimated Heart Rate: " << hr << " (BPM)" << std::endl;
   
   return 0;
 }
