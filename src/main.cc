bbfed7e1f77da84c3b1a9a2cbb380febf9da66e1
Andrew Casper <andrew.jacob.casper@gmail.com>
2019-11-20 23:32:14 -0600

    Rearrange the main routine to call the hb update multiple times.

--- src/main.cc
+++ src/main.cc
@@ -8,19 +8,21 @@ extern double heartbeat_01[5000];
 int main(void) {
 
   // Creates simple data
-  std::vector<int> data;
-  for(int ii = 0; ii < sizeof(heartbeat_01)/4; ii++){
-    data.push_back(heartbeat_01[ii]);
-  }
-  std::cout << "Loaded a " << data.size() << " length data set." << std::endl;
+  //  std::cout << "Loaded a " << data.size() << " length data set." << std::endl;
   argos::hr_params_s hr_params;
   hr_params.sample_rate = 250;
   hr_params.std_dev_threshold = 3.0;
   hr_params.max_heart_rate = 275;
   int hr;
-  hr = argos::EstimateHeartRate(data, hr_params);
 
-  std::cout << "Estimated Heart Rate: " << hr << " (BPM)" << std::endl;
+  for (int jj = 0; jj < 5; ++jj) {
+    std::vector<int> data;
+    for(int ii = 0; ii < 1000; ii++){
+      data.push_back(heartbeat_01[ii + 1000*jj]);
+    }
+    hr = argos::EstimateHeartRate(data, hr_params);
+    std::cout << "Estimated Heart Rate: " << hr << " (BPM)" << std::endl;
+  }
   
   return 0;
 }
